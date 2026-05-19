055c9ac115c62b5ee464f41b3b75dbaefd5e2cd9
RodriTrooCo9 <adelabarra73@gmail.com>
2026-05-19 16:29:38 -0400

    agrado de interacciones

---
+++ app/(routes)/service/page.tsx
@@ -0,0 +1,14 @@
+import CircleImage from "@/components/circle-image";
+import TransitionPage from "@/components/transition-page";
+import { Circle } from "lucide-react";
+
+const ServicesPage = () => {
+    return (
+        <>
+        <TransitionPage />
+        <CircleImage />
+        </>
+    );
+}
+
+export default ServicesPage;
